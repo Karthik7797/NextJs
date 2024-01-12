cc40fea9f13d0cd9ab0593f5f75d8362e9c1f7c6
karthik <karthiknormie@gmail.com>
2024-01-12 22:55:11 +0530

    Initial commit

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+    return <div>AdminPage</div>;
+  };
+  
+  export default AdminPage;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Notfound = () => {
+    return (
+        <>
+        <h2>Not found</h2>
+    <p>sorry</p>
+    <Link href="/">Return Home</Link>
+    </>
+    );
+  };
+  
+  export default Notfound;
