470aaeadfa0bbacb29cc65a107662f0b0d7ab2bc
karthik <karthiknormie@gmail.com>
2024-01-22 20:46:34 +0530

    lastest

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,32 +1,64 @@
+"use client"
 import Link from "next/link";
+import styles from "./links.module.css";
+import Navlink from "./navlink/navLink";
+import { useState } from "react";
 
+const links = [
+  {
+    title: "Homepage",
+    path: "/"
+  },
+  {
+    title: "About",
+    path: "/about"
+  },
+  {
+    title: "Contact",
+    path: "/contact"
+  },
+  {
+    title: "Blog",
+    path: "/blog"
+  }
+];
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/"
-    },
-    {
-      title: "About",
-      path: "/about"
-    },
-    {
-      title: "Contact",
-      path: "/contact"
-    },
-    {
-      title: "Blog",
-      path: "/blog"
-    }
-  ];
+const[open, setOpen] = useState(false);
+const session = true;
+const isAdmin = true;
 
   return (
-    <div>
+    <div className={styles.container}>
+    <div className={styles.links}>
       {links.map(link => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+       
+        <Navlink item={link} key={link.title}/>
+      ))}{
+        session ? (
+          <>
+          {
+            isAdmin && (
+              <Navlink item={{title: "Admin", path: "/admin"}} />
+            )
+          }
+          <button className={styles.logout}>Logout</button>
+          </> 
+        ) : (
+          <Navlink item={{title: "Login", path: "/login"}} />
+        )
+      }
+    </div>
+    <button className={styles.menuButton} onClick={() => {setOpen((prev) => !prev)}}>menu</button>
+    {open && (
+      <div className={styles.mobileLinks}>
+         {links.map(link => (
+       
+       <Navlink item={link} key={link.title}/>
+     ))}
+      </div>
+    )
+
+    }
     </div>
   );
 };
---
+++ src/components/navbar/links/navlink/navLink.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Link from 'next/link'
+import styles from './navlink.module.css'
+import { usePathname } from 'next/navigation'
+
+const Navlink = ({item}) => {
+    const pathname = usePathname();
+    return(
+        <Link href={item.path} className={`${styles.container} ${pathname===item.path && styles.active}`}>
+        {item.title}
+      </Link>
+    )
+}
+
+export default Navlink
